a313e0a062c708ebb9e682bc1e270c9414c193d3
pollyyana <pollyana.medeirosk@gmail.com>
2023-02-01 17:15:06 -0300

    criando 1 helpers

---
+++ delivery_app/lib/app/core/ui/helpers/size_extensions.dart
@@ -0,0 +1,11 @@
+// import 'dart:html'
+
+import 'package:flutter/material.dart';
+
+extension SizeExtensions on BuildContext {
+  double get screenWidth => MediaQuery.of(this).size.width;
+  double get screenHeight => MediaQuery.of(this).size.height;
+
+  double percentWith(double percent) => screenWidth * percent;
+  double percentHeight(double percent) => screenHeight * percent;
+}
--- delivery_app/lib/app/pages/splash/splash_page.dart
+++ delivery_app/lib/app/pages/splash/splash_page.dart
@@ -1,8 +1,8 @@
-import 'package:delivery_app/app/core/ui/styles/app_styles.dart';
+// import 'package:delivery_app/app/core/ui/styles/app_styles.dart';
+import 'package:delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:delivery_app/app/core/ui/styles/colors_app.dart';
 import 'package:delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
-
 import '../../core/config/env/env.dart';
 
 class SplashPage extends StatelessWidget {
@@ -25,6 +25,21 @@ class SplashPage extends StatelessWidget {
             height: 200,
             label: Env.ins ['backend_base_url'] ?? '', 
             onpressed: (){},
+            ),
+            Text(context.screenWidth.toString()),
+            Row(
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: context.percentWith(.6),
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: MediaQuery.of(context).size.width *.4 ,
+                  height: 200,
+                ),
+              ],
             ),
           TextFormField(
             decoration: const InputDecoration(labelText: 'testando'),
