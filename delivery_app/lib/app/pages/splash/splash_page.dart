d65ce17a42cc79c558963c0c4b01bff6ef35aaee
pollyyana <pollyana.medeirosk@gmail.com>
2023-02-01 20:37:19 -0300

    finalizado construção tela SplashPage

--- delivery_app/lib/app/pages/splash/splash_page.dart
+++ delivery_app/lib/app/pages/splash/splash_page.dart
@@ -1,9 +1,7 @@
-// import 'package:delivery_app/app/core/ui/styles/app_styles.dart';
 import 'package:delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:delivery_app/app/core/ui/styles/colors_app.dart';
 import 'package:delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
-import '../../core/config/env/env.dart';
 
 class SplashPage extends StatelessWidget {
 //splah page e o inicio
@@ -14,38 +12,42 @@ class SplashPage extends StatelessWidget {
     context.colors.primary;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Spash'),
-      ),
-      body: Column(
-        children: [
-          Container(),
-          DeliveryButton(
-            width: 200,
-            height: 200,
-            label: Env.ins ['backend_base_url'] ?? '', 
-            onpressed: (){},
+      body: ColoredBox(
+        color: const Color(0XFF140E0E),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.screenWidth,
+                child: Image.asset(
+                  'assets/images/lanche.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-            Text(context.screenWidth.toString()),
-            Row(
-              children: [
-                Container(
-                  color: Colors.red,
+            Center(
+              child: Column(children: [
+                SizedBox(
+                  height: context.percentHeight(.30),
+                ),
+                Image.asset(
+                  'assets/images/logo.png',
+                ),
+                const SizedBox(
+                  height: 80,
+                ),
+                DeliveryButton(
                   width: context.percentWith(.6),
-                  height: 200,
+                  height: 35,
+                  label: 'ACESSAR',
+                  onpressed: () {},
                 ),
-                Container(
-                  color: Colors.blue,
-                  width: MediaQuery.of(context).size.width *.4 ,
-                  height: 200,
-                ),
-              ],
+              ]),
             ),
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'testando'),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
-} 
+}
