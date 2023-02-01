7feb14e5061ef9b345a44f9fee6caef3ea155855
pollyyana <pollyana.medeirosk@gmail.com>
2023-02-01 17:14:07 -0300

    ambiente RestClient

---
+++ delivery_app/lib/app/core/rest_client/custom_dio.dart
@@ -0,0 +1,23 @@
+import 'package:delivery_app/app/core/config/env/env.dart';
+import 'package:dio/dio.dart';
+import 'package:dio/native_imp.dart';
+
+class CustomDio extends DioForNative {
+  CustomDio()
+      : super(BaseOptions(
+            baseUrl: Env.ins['backend_base_url'] ?? '',
+            connectTimeout: 5000,
+            receiveTimeout: 60000)) {
+    interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+      requestHeader: true,
+    ));
+  }
+  CustomDio auth(){
+    return this;
+  }
+  CustomDio unauth(){
+    return this;
+  }
+}
