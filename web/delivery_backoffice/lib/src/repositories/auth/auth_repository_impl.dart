d9ff4f899922e6551839e5e29a58cac4ad018ee9
pollyyana <pollyana.medeirosk@gmail.com>
2023-05-09 15:18:02 -0300

    implementando AUTH DATA LAYER

---
+++ web/delivery_backoffice/lib/src/repositories/auth/auth_repository_impl.dart
@@ -0,0 +1,31 @@
+import 'dart:developer';
+import 'package:dio/dio.dart';
+import '../../core/exceptions/repository_exceptions.dart';
+import '../../core/exceptions/unathorize_exception.dart';
+import '../../core/rest_client/custom_dio.dart';
+import '../../models/auth_model.dart';
+import './auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final CustomDio _dio;
+  AuthRepositoryImpl(this._dio);
+  @override
+  Future<AuthModel> login(String email, String password) async {
+    try {
+      final result = await _dio.unauth().post('/auth', data: {
+        'email': email,
+        'password': password,
+        'admin': true,
+      });
+
+      return AuthModel.fromMap(result.data);
+    } on DioError catch (e, s) {
+      if (e.response?.statusCode == 430) {
+        log('login ou senha invalidos', error: e, stackTrace: s);
+        throw UnathorizeException();
+      }
+      log('erro ao realizar login', error: e, stackTrace: s);
+      throw RepositoryException(message: 'Erro ao realizar login');
+    }
+  }
+}
