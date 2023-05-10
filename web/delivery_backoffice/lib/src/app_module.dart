faa1cb6a3e23c06191a0a4e179a236ae588ad9c7
pollyyana <pollyana.medeirosk@gmail.com>
2023-05-10 19:17:21 -0300

    implementando base route com RouteOutlet

--- web/delivery_backoffice/lib/src/app_module.dart
+++ web/delivery_backoffice/lib/src/app_module.dart
@@ -2,6 +2,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'modules/core/core_module.dart';
 import 'modules/home/home_page.dart';
 import 'modules/login/login.module.dart';
+import 'modules/template/base_layout.dart';
 
 class AppModule extends Module {
   @override
@@ -12,6 +13,15 @@ class AppModule extends Module {
   @override
   List<ModularRoute> get routes => [
         ModuleRoute('/login', module: LoginModule()),
-        ChildRoute('/home', child: (context, args) => const HomePage()),
+        ChildRoute(
+          '/',
+          child: (context, args) => const BaseLayout(
+            body: RouterOutlet(),
+          ),
+          children: [
+            ChildRoute('/home', 
+            child: (context, args) => const HomePage(),),
+          ],
+        ),
       ];
 }
--- web/delivery_backoffice/lib/src/modules/home/home_page.dart
+++ web/delivery_backoffice/lib/src/modules/home/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../../core/ui/helpers/loader.dart';
 import '../../core/ui/helpers/messages.dart';
-import '../template/base_layout.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,8 +13,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with Loader, Messages {
   @override
   Widget build(BuildContext context) {
-    return BaseLayout(
-      body: Container(
+    return Container(
         child: Container(
           child: Column(
             children: [
@@ -36,7 +34,7 @@ class _HomePageState extends State<HomePage> with Loader, Messages {
             ],
           ),
         ),
-      ),
+      
     );
   }
 }
