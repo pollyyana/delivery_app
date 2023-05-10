a0f9becd3a13b7d70acc80846ee4cb3b1d958eac
pollyyana <pollyana.medeirosk@gmail.com>
2023-05-10 00:06:07 -0300

    subindo arquivos

--- web/delivery_backoffice/lib/src/models/auth_model.dart
+++ web/delivery_backoffice/lib/src/models/auth_model.dart
@@ -1,20 +1,20 @@
 import 'dart:convert';
 
 class AuthModel {
-  final String acessToken;
+  final String accessToken;
   AuthModel({
-    required this.acessToken,
+    required this.accessToken,
   });
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
-      'acess_token': acessToken,
+      'access_token': accessToken,
     };
   }
 
   factory AuthModel.fromMap(Map<String, dynamic> map) {
     return AuthModel(
-      acessToken: map['acess_token'] as String,
+      accessToken: map['access_token'] as String,
     );
   }
 
