c1412bd5e7119c1d5f8fe2b51e9058427a26f0ff
pollyyana <pollyana.medeirosk@gmail.com>
2023-05-27 20:38:40 -0300

    baseHeader criado

--- web/delivery_backoffice/lib/src/modules/payment_type/payment_type_page.dart
+++ web/delivery_backoffice/lib/src/modules/payment_type/payment_type_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/payment_type_headers.dart';
+
 class PaymentTypePage extends StatelessWidget {
   const PaymentTypePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Payment type'),
+    return  Container(
+      color: Colors.grey[50],
+      padding: const EdgeInsets.only(left: 40, top:  40),
+      child: Column(
+        children: const  [
+           PaymentTypeHeader()
+        ],
+      ),
     );
   }
 }
